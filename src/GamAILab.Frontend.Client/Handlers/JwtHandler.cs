f00f7099bfc6d3ca9907bd3e7b30d64e47434efa
Mawiel <andreasnordboe@gmail.com>
2026-08-04 09:17:08 +0100

    Added automatic frontend logout handling for when requests return 401 Unauthorised

--- src/GamAILab.Frontend.Client/Handlers/JwtHandler.cs
+++ src/GamAILab.Frontend.Client/Handlers/JwtHandler.cs
@@ -1,11 +1,16 @@
+using System.Net;
 using System.Net.Http.Headers;
 using Blazored.LocalStorage;
+using GamAILab.Frontend.Client.Providers;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace GamAILab.Frontend.Client.Handlers;
 
 public sealed class JwtHandler : DelegatingHandler
 {
     private readonly ILocalStorageService _localStorage;
+    private readonly IServiceProvider _serviceProvider;
 
     public JwtHandler(ILocalStorageService localStorage)
     {
@@ -20,7 +25,23 @@ public sealed class JwtHandler : DelegatingHandler
         {
             request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", jwtToken);
         }
+
+        // Logout user once requests start returning 401 unauthorised, there could potentially also be a warning before logout using Snackbar
+        var response = await base.SendAsync(request, cancellationToken);
+        if (response.StatusCode == HttpStatusCode.Unauthorized)
+        {
+            var navigationManager = _serviceProvider.GetRequiredService<NavigationManager>();
+            var authStateProvider = _serviceProvider.GetService<AuthenticationStateProvider>() as JWTAuthenticationStateProvider;
+            if (authStateProvider is not null)
+            {
+                await authStateProvider.SetUserLoggedOut();
+            }
+            
+            navigationManager.NavigateTo("/login", forceLoad: true);
+        }
         
-        return await base.SendAsync(request, cancellationToken);
+        return response;
     }
+    
+    
 }
